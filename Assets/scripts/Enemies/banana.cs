87dead718033697fa1a68ba5b3c97f56289eaadc
MKKLBRFFLT <mikkelreffelt@gmail.com>
2024-04-17 23:10:10 +0200

    Bananlyd

--- Assets/scripts/Enemies/banana.cs
+++ Assets/scripts/Enemies/banana.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class banana : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private AudioSource audioPlayer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.transform.Find("Target") && coll.transform.Find("Target").TryGetComponent<PlayerCounts>(out PlayerCounts pComp))
         {
             pComp.bananaAmount += 1;
-            Destroy(gameObject);
+            spriteRenderer.enabled = false;
+            audioPlayer.Play();
         }
+
+
+    }
+    private void PlayAudioAndDestroy()
+    {
+        audioPlayer.Play();
+        StartCoroutine(DestroyAfterDelay(audioPlayer.clip.length));
+    }
+
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
