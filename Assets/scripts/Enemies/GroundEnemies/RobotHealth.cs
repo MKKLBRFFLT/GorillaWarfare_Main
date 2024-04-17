738718b9c430ab2f69b27bf9e401797b39b980e3
MKKLBRFFLT <mikkelreffelt@gmail.com>
2024-04-17 22:41:52 +0200

    Robot dødsanimation

--- Assets/scripts/Enemies/GroundEnemies/RobotHealth.cs
+++ Assets/scripts/Enemies/GroundEnemies/RobotHealth.cs
@@ -7,9 +7,12 @@ public class RobotHealth : MonoBehaviour
     [Header("Ints")]
     readonly int maxHealth = 3;
     public int health;
+    private float yOffset = 0.4f;
 
     [Header("GameObjects")]
     [SerializeField] GameObject banana;
+    [SerializeField] GameObject deathAnimation;
+    [SerializeField] GameObject robotHead;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,10 @@ public class RobotHealth : MonoBehaviour
     {
         if (health <= 0)
         {
+
+            Vector3 headPosition = transform.position + Vector3.up * yOffset; // Offset the position slightly above
+            Instantiate(deathAnimation, transform.position, Quaternion.identity);
+            Instantiate(robotHead, headPosition, Quaternion.identity); // Instantiate the robotHead at the adjusted position
             Instantiate(banana, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
